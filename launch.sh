6c2466fc901a0547c8eb6b6fefc4613f492e3025
Keith Welch <keith@paralogiktech.com>
2019-08-26 23:36:25 -0400

    Use xset to disable screen blanking/saving

--- launch.sh
+++ launch.sh
@@ -10,8 +10,16 @@ fi
 if [ -z $URL ] || [ "$URL" == "error" ]; then
 	URL="file://`pwd`/config_error.html"
 fi
+# kill all open chromium browsers
 pkill -f -- "chromium-browser"
 sleep 2
+# open display html
 /usr/bin/chromium-browser --app="$URL" > /dev/null 2>&1 &
 sleep 3
+# Focus last created chromium window and full screen
 xdotool windowactivate --sync $(xdotool search --onlyvisible --class chromium-browser | tail -1) key F11
+# Disable screen going black and screen turn off
+xset s noblank
+xset s off
+# Disable Energy Star which would also turn off screen
+xset -dpms
